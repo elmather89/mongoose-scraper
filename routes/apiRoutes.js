b3cf0cbd7dc09cc5dcd2cadecc2d2fc573e339b9
Sansevieria <elizabethleighmather@gmail.com>
2019-06-27 00:19:07 -0500

    axios get returning responses

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,25 +5,44 @@ var axios = require("axios");
 var moment = require('moment');
 var cheerio = require("cheerio");
 
-module.exports = function(app) {
-    app.get("/scrape", function(req, res) {
-        axios.get("https://local.theonion.com/").then(function(response) {
-            console.log(response);
-            
+module.exports = function (app) {
+
+    app.get("/scrape", function (req, res) {
+
+        axios.get("https://local.theonion.com/").then(function (response) {
+            // console.log(response);
+
             var $ = cheerio.load(response.data);
+            // console.log($);
 
-            // $("cw4lnv-6 bqxOdn sc-759qgu-0 jJoMDo h1").each(function(i, element) {
-            //     var result = {};
 
-            //     result.title = $(this).children("a"),text("");
-            //     result.link = $(this).children("a").attr("href");
-            // });
+            $("article h1").each(function (i, element) {
+                var result = {};
+                // console.log(result);
+
+                result.title = $(this).text();
+                // console.log($(this).text());
+                result.link = $(this).parent("a").attr("href");
+                // console.log($(this).parent("a").attr("href"));
+
+                db.Article.create(result).then(function (dbArticle) {
+                    console.log(dbArticle);
+                }).catch(function (err) {
+                    console.log(err);
+                });
+
+            });
+
+        });
+
+    });
+
+    app.get("/articles", function (req, res) {
+        db.Article.find({}).then(function(dbArticle) {
+            res.json(dbArticle);
+        }).catch(function(err) {
+            res.json(err);
         });
     });
 
-    app.get("/articles", function(req, res) {
-        //.
-    });
-
-
-}
+};
