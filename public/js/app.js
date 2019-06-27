f062a39c781a0ebd567356669b508e692b732889
Sansevieria <elizabethleighmather@gmail.com>
2019-06-27 01:17:17 -0500

    submit btn id fixed

--- public/js/app.js
+++ public/js/app.js
@@ -42,3 +42,31 @@ $.getJSON("/articles", function(data) {
       });
   });
   
+  $(document).on("click", "#savecomment", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+  
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#titleinput").val(),
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
+      }
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        // Empty the comments section
+        $("#comments").empty();
+      });
+  
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
+  
